a1af493c6fdff652f2a53d736668095e5253f9e7
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-07-03 12:45:27 -0700

    elevator command + refactoring shooter stuff and constants

--- src/main/java/frc/robot/subsystems/ElevatorSubs/Elevator.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubs/Elevator.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.Neo;
 import frc.robot.util.Constants.TrapConstants;
@@ -61,8 +62,14 @@ public class Elevator extends SubsystemBase {
         leftElevator.setTargetPosition(pos);
     }
 
+    public Command setPositionCommand(double pos) {
+        return Commands.runOnce(() -> this.setPosition(pos))
+            .andThen(Commands.waitUntil(this.isAtTargetPosition()));
+    }
+
     public Command stop() {
-        return runOnce(() -> leftElevator.set(0));
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
     }
 
 }
